3ec93a232f1e6f9bdae0648ee7d6dc6a1993b0c6
Junaid Farooq <junaid@evercam.io>
2019-07-18 22:40:09 +0500

    Added where and orderBy using firebase store index property.

--- scripts/chat.js
+++ scripts/chat.js
@@ -18,6 +18,8 @@ class Chatroom {
   }
   getChats(callback) {
     this.chats
+      .where("room", "==", this.room)
+      .orderBy("created_at")
       .onSnapshot(snapshot => {
         snapshot.docChanges().forEach(change => {
           if (change.type === "added") {
